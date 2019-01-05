2c1c417d791ef0e433f35ca11e2d24806fcffc19
Neil Vodoor <nvodoor@gmail.com>
2019-01-05 12:05:34 -0800

    Webpack configured to handle .js and .jsx file extensions

---
+++ src/client/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  <div>
+    <h1>Example React App</h1>
+  </div>
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/client/app.jsx',
+  output: {
+    filename: '[hash].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
